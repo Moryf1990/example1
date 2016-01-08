88c75fb21a315cbfdd3020ad1c5bba426c242db6
Mory Friedman <friedmanmory2@gmail.com>
2016-01-08 17:13:27 -0600

    Final Commit

--- scripts/main.js
+++ scripts/main.js
@@ -36,3 +36,67 @@ var thumbnails = [
 
 
 
+
+
+// This is the code for Question 8. I have it commented out because it doesn't work and 
+// because it stops the code below from running.
+
+// getImages(function(thumbnails) {
+// 	thumbnails.preventDefault();
+	// var caption = ('');
+	// var image = ('');
+	// 	var url = ('');
+
+// 	for (var i = 0; i < thumbnails.length; i++) {
+// 		caption.show();
+// 		image.show();
+// 		<a>url.show()</a>;
+// 	}
+
+// 	url.on('click', function() {
+// 		if (caption.val() === '' && image.val() === '' && url.val() === '') {
+// 		window.location = url.val();
+// 		}
+// 	});
+// });
+
+
+
+// Below are some other attempts at solving Question 8
+
+// thumbnails.forEach(function(caption, image, url) {
+// 	console.log(cpation.id + image.id + url.id);
+// })
+
+
+
+// for (var i = 0; i < thumbnails.length; i++) {
+// 	var caption = caption[i];
+// 	var image = image[i];
+// 	caption.show();
+// 	image.show();
+// }
+
+
+
+
+
+
+var readMore = $('.read-more');
+var more = $('.more');
+
+readMore.on('click', function() {
+	more.slideToggle('fast', function() {
+		if ($(this).is(':visible')) {
+            readMore.html('read less...')               
+        } else {
+             readMore.html('read more...')                
+        }        
+    });      
+});
+
+
+
+
+
+
